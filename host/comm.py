7e4605a51b6306b1559b8330bd3ebb33f8fa63ed
Ayodeji Ige <ayodeji18@outlook.com>
2017-10-10 09:06:47 -0400

    Added joystick and serial libraries. Added main application

---
+++ host/comm.py
@@ -0,0 +1,56 @@
+import serial
+import time
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+class SerialCom(object):
+    """docstring for SerialCom"""
+    START = 0xFE
+    FORWARD = 0x01
+    UP = 0x02
+    DOWN = 0x03
+    LEFT = 0x04
+    RIGHT = 0x05
+    MIN_MAG = 0x00
+    MAX_MAG = 0xFA
+    END = 0xFF
+    def __init__(self, port="COM4", baudrate=9600):
+        super(SerialCom, self).__init__()
+        self._port = port
+        self._baudrate = baudrate
+        self._log = logging.getLogger(self.__class__.__name__)
+        try:
+            self._com = serial.Serial(self._port, baudrate=self._baudrate)
+        except Exception as e:
+            self._log.error(e)
+
+    def send_direction(self, direction, magnitude):
+        if ((type(direction) != int) & (type(magnitude) != int)):
+            self._log.error("Direction or magnitude wrong type")
+            return
+        if((direction > self.RIGHT) | (direction < self.FORWARD)):
+            self._log.error("Wrong directions")
+            return
+        if((magnitude < self.MIN_MAG) | (magnitude > self.MAX_MAG)):
+            self._log.error("Wrong magnitude")
+            return
+        command = bytearray([direction, magnitude])
+        self._com.write(command)
+        self._log.info("Sending %d %d", int(command[0]), int(command[1]))
+
+    def send(self, data):
+        data = [self.START] + data + [self.END]
+        command = bytearray(data)
+        self._com.write(command)
+        command_print = " ".join(str(x) for x in data)
+        self._log.info("Sending %s", command_print)
+
+def main():
+    com = SerialCom()
+    for i in range(6):
+        for j in range(248, 255):
+            com.send_direction(i, j)
+            time.sleep(0.1)
+if __name__ == '__main__':
+    main()
---
+++ host/main.py
@@ -0,0 +1,46 @@
+from joystick import Controller
+from comm import SerialCom
+import time
+
+global left_axis
+global right_axis
+def main():
+    # Initialise
+    joy = Controller(0)
+    com = SerialCom()
+    moved = False
+    n_reps = 0
+    out_prev = [10, 10, 10]
+    global left_axis
+    global right_axis
+    try:
+        while True:
+            # Pump function is needed to receive input
+            joy.pump()
+            # Get joystick controls
+            # z = up and down
+            # x = forward and back
+            # y = left and right
+            useless, z_axis = joy.get_left_stick()
+            y_axis, x_axis = joy.get_right_stick()
+
+            out = [int(-10*x_axis)+10, int(10*y_axis)+10, int(-10*z_axis)+10]
+
+            if out[0] == out_prev[0] and  out[1] == out_prev[1] and  out[2] == out_prev[2]:
+                n_reps += 1
+            else:
+                n_reps = 0
+
+            if n_reps <= 10:
+                com.send(out)
+
+            out_prev = out
+            time.sleep(0.01)
+
+
+
+    except KeyboardInterrupt:
+        print('interrupted!')
+
+if __name__ == '__main__':
+    main()
